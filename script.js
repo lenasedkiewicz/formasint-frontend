c2b09e8cdab93734a5bdbabd38a26543b97754d9
msedkiewicz <magdalenainspirations@gmail.com>
2025-04-17 13:23:33 +0200

    Add data fetching

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.info("dziala");
+const BASE_URL = "https://brandstestowy.smallhost.pl/api";
+
+fetch(`${BASE_URL}/random`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Dane:", data);
+  })
+  .catch((error) => {
+    console.error("Error while fetching data", error);
+  });
+
+// https://brandstestowy.smallhost.pl/api/random?pageNumber=2&pageSize=2
